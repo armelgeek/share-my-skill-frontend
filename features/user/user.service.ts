fef8adc42c8e625809296fdecaf6cc3a34584e39
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-10 23:22:42 +0300

    feat: user.service.ts terminé, prêt pour la prochaine tâche

--- features/user/user.service.ts
+++ features/user/user.service.ts
@@ -2,4 +2,4 @@ import BaseService from '@/shared/lib/services/base-service';
 import { API_ENDPOINTS } from '@/shared/config/api';
 import type { User } from './user.schema';
 
-export const userService = new BaseService(API_ENDPOINTS.user);
+export const userService = new BaseService<User>(API_ENDPOINTS.user);
