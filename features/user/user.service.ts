370e10464f6aed0ab20876b02fe358a5adfed9b7
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-10 23:25:06 +0300

    fix: correction du service user, typage et endpoint base OK

--- features/user/user.service.ts
+++ features/user/user.service.ts
@@ -1,5 +1,4 @@
 import BaseService from '@/shared/lib/services/base-service';
 import { API_ENDPOINTS } from '@/shared/config/api';
-import type { User } from './user.schema';
 
-export const userService = new BaseService<User>(API_ENDPOINTS.user);
+export const userService = new BaseService(API_ENDPOINTS.user.base);
