f169b99349862cae16d5f7e2c8fa7a079443d99e
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-12 10:02:07 +0300

    feat(groupe): hook useGroupe pour React Query

---
+++ features/groupe/hooks/use-groupe.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { groupeService } from '../groupe.service';
+
+export function useGroupe() {
+  return useQuery({
+    queryKey: ['groupes'],
+    queryFn: () => groupeService.list(),
+  });
+}
