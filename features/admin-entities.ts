20f24bc8e762c092d96f42b06101ccc5feb15e73
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-13 00:15:53 +0300

    Ajout de l'entité Ressource à la sidebar admin et correction de la syntaxe (tâche 42)

--- features/admin-entities.ts
+++ features/admin-entities.ts
@@ -1,4 +1,6 @@
 import { AtelierAdminConfig } from './atelier/atelier.admin-config';
+import { MessageAdminConfig } from './message/message.admin-config';
+import { RessourceAdminConfig } from './ressource/ressource.admin-config';
 
 export const adminEntities = [
   {
@@ -6,6 +8,18 @@ export const adminEntities = [
     href: '/admin/atelier',
     config: AtelierAdminConfig,
     menuOrder: 2,
+  },
+  {
+    path: 'message',
+    href: '/admin/message',
+    config: MessageAdminConfig,
+    menuOrder: 3,
+  },
+  {
+    path: 'ressource',
+    href: '/admin/ressource',
+    config: RessourceAdminConfig,
+    menuOrder: 4,
   },
   // ...autres entités admin
 ];
