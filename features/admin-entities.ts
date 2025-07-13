d3467a248d8ebc346d7b55fe6b8988541c89b498
Hajavololona Armellin <armelgeek5@gmail.com>
2025-07-13 06:58:40 +0300

    Ajout de l'entité Badge à la sidebar admin et correction de la syntaxe (tâche 50)

--- features/admin-entities.ts
+++ features/admin-entities.ts
@@ -1,7 +1,7 @@
 import { AtelierAdminConfig } from './atelier/atelier.admin-config';
 import { MessageAdminConfig } from './message/message.admin-config';
 import { RessourceAdminConfig } from './ressource/ressource.admin-config';
-
+import { BadgeAdminConfig } from './badge/badge.admin-config';
 export const adminEntities = [
   {
     path: 'atelier',
@@ -20,6 +20,12 @@ export const adminEntities = [
     href: '/admin/ressource',
     config: RessourceAdminConfig,
     menuOrder: 4,
+  },
+  {
+    path: 'badge',
+    href: '/admin/badge',
+    config: BadgeAdminConfig,
+    menuOrder: 5,
   },
   // ...autres entités admin
 ];
